2d43d4490f57a1bdf619922b99c8bd53a519a5dd
jonahcastro <jonahmay.castro08@gmail.com>
2022-08-05 03:37:55 +0800

    Fixed prettier error

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,8 @@
-import '../styles/fonts.css'
-import '../styles/tailwind.css'
+import "../styles/fonts.css";
+import "../styles/tailwind.css";
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default App
+export default App;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,11 @@
-import { Fragment } from 'react'
-import Head from 'next/head'
-import Header from '../components/Header'
-import Banner from '../layouts/Banner'
-import About from '../layouts/About'
-import Services from '../layouts/Services'
-import Contact from '../layouts/Contact'
-import Footer from '../components/Footer'
+import { Fragment } from "react";
+import Head from "next/head";
+import Header from "../components/Header";
+import Banner from "../layouts/Banner";
+import About from "../layouts/About";
+import Services from "../layouts/Services";
+import Contact from "../layouts/Contact";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -15,7 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Oneday</title>
       </Head>
-      <Header/>
+      <Header />
       <main>
         <Banner />
         <About />
@@ -24,5 +24,5 @@ export default function Home() {
       </main>
       <Footer />
     </Fragment>
-  )
+  );
 }
