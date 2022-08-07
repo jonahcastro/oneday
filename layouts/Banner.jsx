3e74a8b7a1e728692148ff2acb86cee486cd54bd
jonahcastro <jonahmay.castro08@gmail.com>
2022-08-07 23:47:46 +0800

    Newsletter API using sendinblue

--- layouts/Banner.jsx
+++ layouts/Banner.jsx
@@ -1,10 +1,45 @@
-import { useRouter } from "next/router";
 import { MailIcon } from "@heroicons/react/outline";
 import { motion } from "framer-motion";
+import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Banner() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset
+  } = useForm({ mode: "onBlur", defaultValues: { Email: "" }  });
+
+  async function onSubmit(data, e) {
+    
+    await fetch('/api/mail', {
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+    .then((response) => response.json())
+    //Then with the data from the response in JSON...
+    .then((result) => {
+      if(result.status == 'success'){
+        toast.success(result.message, {
+          position: toast.POSITION.TOP_RIGHT
+        });
+      }else{
+        toast.error(result.message, {
+          position: toast.POSITION.TOP_RIGHT
+        });
+      }
+      
+    })
+    e.target.reset();
+    
+  }
+
   return (
     <section className="container">
+      <ToastContainer />
       <div className="my-10 md:my-20">
         <div className="grid grid-cols-1 items-center justify-center gap-8 lg:grid-cols-2">
           <motion.div
@@ -27,23 +62,44 @@ export default function Banner() {
                 Estate planning for the new age. Setup trusts quickly,
                 affordably and securely from the comfort of your own home.
               </p>
-              <div className="mt-8 flex-none gap-4 md:flex">
+
+              <form
+                method="post"
+                onSubmit={handleSubmit(onSubmit)}
+                className="mt-8 flex-none gap-4 md:flex"
+              >
                 <label className="relative block">
                   <span className="sr-only">Stay up to date</span>
                   <span className="absolute inset-y-0 left-0 flex items-center pl-8">
                     <MailIcon className="h-5 w-5 text-secondary/60" />
                   </span>
                   <input
-                    className="block w-full rounded-full border border-slate-200 bg-white py-3 pl-14 pr-10 shadow-sm placeholder:text-sm placeholder:text-secondary/60 focus:border-purple focus:outline-none focus:ring-1 focus:ring-purple sm:text-sm md:w-80"
+                    className={`block w-full rounded-full border bg-white py-3 pl-14 pr-10 shadow-sm placeholder:text-sm placeholder:text-secondary/60  sm:text-sm md:w-80 ${
+                      errors.Email
+                        ? "border-red-400 text-red-300"
+                        : "text-purple-200 border-purple-400"
+                    }`}
                     placeholder="Stay up to date"
                     type="text"
-                    name="email"
+                    {...register("Email", {
+                      required: "Email Address is required",
+                      pattern: {
+                        value: /^\S+@\S+$/i,
+                        message: "Invalid email address",
+                      },
+                    })}
                   />
                 </label>
-                <button className="mt-3 w-full rounded-full border bg-purple px-6 py-3 text-sm uppercase tracking-wide text-white hover:bg-purple/90 hover:shadow-md md:mt-0 md:w-auto">
+
+                <button
+                  className={`mt-3 w-full rounded-full border bg-purple px-6 py-3 text-sm uppercase tracking-wide text-white hover:bg-purple/90 hover:shadow-md md:mt-0 md:w-auto`}
+               >
                   subscribe
                 </button>
-              </div>
+              </form>
+              <span className="mt-2 ml-10 text-xs font-medium tracking-wide text-red-500">
+                {errors.Email?.message}
+              </span>
             </div>
           </motion.div>
           <motion.div
