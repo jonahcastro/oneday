6f1ca844d17de49d81daf657899b49c5f62288e7
Julian <jt_hk@outlook.com>
2022-09-24 01:22:06 +0800

    run lint and style for new changes

--- pages/blog/what-is-trust.jsx
+++ pages/blog/what-is-trust.jsx
@@ -14,46 +14,82 @@ export default function Blog() {
       <Header />
       <main>
         <div role="gridcell">
-            <div className="px-4 lg:px-40 lg:mt-48">
-                <h5 className="font-display text-3xl font-bold leading-8 text-purple md:text-5xl lg:text-4xl lg:leading-12 mt-20 mx-5">
-                What is a trust?
-                </h5>
+          <div className="px-4 lg:mt-48 lg:px-40">
+            <h5 className="mx-5 mt-20 font-display text-3xl font-bold leading-8 text-purple md:text-5xl lg:text-4xl lg:leading-12">
+              What is a trust?
+            </h5>
 
-                <p className="mx-5 mt-5 font-sans text-md leading-5  text-secondary md:text-lg lg:mx-10 lg:mt-10 lg:leading-7 ">
-                A trust is not a company nor an organisation, but a relationship that you establish with someone, whether it be a family member, relative or friend, or a professional, whom you trust and feel comfortable with in transferring your assets to, for the purposes of holding your assets for the benefit of your loved ones. 
-                </p>
-                
-                <h5 className="font-display text-3xl font-bold leading-8 text-purple md:text-5xl lg:text-4xl lg:leading-12 mt-10 mx-5">
-                Who are the parties involved in establishing a trust? 
-                </h5>
+            <p className="text-md mx-5 mt-5 font-sans leading-5  text-secondary md:text-lg lg:mx-10 lg:mt-10 lg:leading-7 ">
+              A trust is not a company nor an organisation, but a relationship
+              that you establish with someone, whether it be a family member,
+              relative or friend, or a professional, whom you trust and feel
+              comfortable with in transferring your assets to, for the purposes
+              of holding your assets for the benefit of your loved ones.
+            </p>
 
-                <p className="mx-5 mt-5 font-sans text-md leading-5  text-secondary md:text-lg lg:mx-10 lg:mt-10 lg:leading-7 ">
-                You, who will be transferring assets into the trust, are called the “settlor”. The person or professional whom you are transferring your assets to is called the “trustee”. Your loved ones, who will receive benefits from your trust, are called “beneficiaries”. 
-                </p>
+            <h5 className="mx-5 mt-10 font-display text-3xl font-bold leading-8 text-purple md:text-5xl lg:text-4xl lg:leading-12">
+              Who are the parties involved in establishing a trust?
+            </h5>
 
-                <h5 className="font-display text-3xl font-bold leading-8 text-purple md:text-5xl lg:text-4xl lg:leading-12 mt-10 mx-5">
-                I hear people mentioning bare or simple trust and discretionary trust? What is the difference? 
-                </h5>
+            <p className="text-md mx-5 mt-5 font-sans leading-5  text-secondary md:text-lg lg:mx-10 lg:mt-10 lg:leading-7 ">
+              You, who will be transferring assets into the trust, are called
+              the “settlor”. The person or professional whom you are
+              transferring your assets to is called the “trustee”. Your loved
+              ones, who will receive benefits from your trust, are called
+              “beneficiaries”.
+            </p>
 
-                <p className="mx-5 mt-5 font-sans text-md leading-5  text-secondary md:text-lg lg:mx-10 lg:mt-10 lg:leading-7 ">
-                A bare or simple trust is established if you transfer some assets to a trustee to hold on your behalf. Your trustee will basically listen to what you say and act upon your instructions in relation to how your assets should be managed. Your trustee in these circumstances is effectively your nominee. Although your assets are held in the name of your trustee, such assets still belong to you and your estate in the unfortunate event of you leaving this world. Your assets in a bare trust are therefore not bulletproof from creditors. 
-                </p>
+            <h5 className="mx-5 mt-10 font-display text-3xl font-bold leading-8 text-purple md:text-5xl lg:text-4xl lg:leading-12">
+              I hear people mentioning bare or simple trust and discretionary
+              trust? What is the difference?
+            </h5>
 
-                <h5 className="font-display text-3xl font-bold leading-8 text-purple md:text-5xl lg:text-4xl lg:leading-12 mt-10 mx-5">
-                Discretionary Trust
-                </h5>
+            <p className="text-md mx-5 mt-5 font-sans leading-5  text-secondary md:text-lg lg:mx-10 lg:mt-10 lg:leading-7 ">
+              A bare or simple trust is established if you transfer some assets
+              to a trustee to hold on your behalf. Your trustee will basically
+              listen to what you say and act upon your instructions in relation
+              to how your assets should be managed. Your trustee in these
+              circumstances is effectively your nominee. Although your assets
+              are held in the name of your trustee, such assets still belong to
+              you and your estate in the unfortunate event of you leaving this
+              world. Your assets in a bare trust are therefore not bulletproof
+              from creditors.
+            </p>
 
-                <p className="mx-5 mt-5 font-sans text-md leading-5  text-secondary md:text-lg lg:mx-10 lg:mt-10 lg:leading-7 mb-10">
-                A discretionary trust on the other hand is established if you transfer your assets to a trustee and you then lose all control over such assets, except for certain powers reserved to you. Your trustee will have the absolute discretion on deciding how the transferred assets should be managed and to whom should the assets be distributed to. Assets in a discretionary trust can be protected from creditors if the trust documents are well drafted.<br></br><br></br>
+            <h5 className="mx-5 mt-10 font-display text-3xl font-bold leading-8 text-purple md:text-5xl lg:text-4xl lg:leading-12">
+              Discretionary Trust
+            </h5>
 
-                You might wonder how your trustee might manage the trust asset and whether he will manage in line with your intention.  <br></br><br></br>
-
-                Although you no longer have any say over the trust assets (except for powers reserved to you), you can give your trustee some guidance through a letter of wishes on how the assets should be managed and distributed. For example, in the letter of wishes, you can specify to whom should the trust assets be distributed to in the unfortunate event of you leaving this world and on what conditions, if any, should they receive their distributions. However, please note that your letter of wishes is not legally binding on your trustee. Your trustee could exercise his discretion not to follow your wishes if he considers that it is not in the best interests of the beneficiaries. <br></br><br></br>
-
-                Hope this newsletter can give you a better understanding of establishing a trust. Stay tuned and look out for our next newsletter!
-
-                </p>
-            </div>
+            <p className="text-md mx-5 mt-5 mb-10 font-sans  leading-5 text-secondary md:text-lg lg:mx-10 lg:mt-10 lg:leading-7">
+              A discretionary trust on the other hand is established if you
+              transfer your assets to a trustee and you then lose all control
+              over such assets, except for certain powers reserved to you. Your
+              trustee will have the absolute discretion on deciding how the
+              transferred assets should be managed and to whom should the assets
+              be distributed to. Assets in a discretionary trust can be
+              protected from creditors if the trust documents are well drafted.
+              <br></br>
+              <br></br>
+              You might wonder how your trustee might manage the trust asset and
+              whether he will manage in line with your intention. <br></br>
+              <br></br>
+              Although you no longer have any say over the trust assets (except
+              for powers reserved to you), you can give your trustee some
+              guidance through a letter of wishes on how the assets should be
+              managed and distributed. For example, in the letter of wishes, you
+              can specify to whom should the trust assets be distributed to in
+              the unfortunate event of you leaving this world and on what
+              conditions, if any, should they receive their distributions.
+              However, please note that your letter of wishes is not legally
+              binding on your trustee. Your trustee could exercise his
+              discretion not to follow your wishes if he considers that it is
+              not in the best interests of the beneficiaries. <br></br>
+              <br></br>
+              Hope this newsletter can give you a better understanding of
+              establishing a trust. Stay tuned and look out for our next
+              newsletter!
+            </p>
+          </div>
         </div>
       </main>
       <Footer />
--- pages/blog/why-oneday.jsx
+++ pages/blog/why-oneday.jsx
@@ -14,55 +14,85 @@ export default function Blog() {
       <Header />
       <main>
         <div role="gridcell">
-            <div className="px-4 lg:px-40 lg:mt-48">
-                <h5 className="font-display text-3xl font-bold leading-8 text-purple md:text-5xl lg:text-4xl lg:leading-12 mt-20 mx-5">
-                    What does OneDay see in the current trust industry?
-                </h5>
+          <div className="px-4 lg:mt-48 lg:px-40">
+            <h5 className="mx-5 mt-20 font-display text-3xl font-bold leading-8 text-purple md:text-5xl lg:text-4xl lg:leading-12">
+              What does OneDay see in the current trust industry?
+            </h5>
 
-                <p className="mx-5 mt-5 font-sans text-md leading-5  text-secondary md:text-lg lg:mx-10 lg:mt-10 lg:leading-7 ">
-                    Given our experience in the trust industry, we see that people consider trusts as: <br></br><br></br>
+            <p className="text-md mx-5 mt-5 font-sans leading-5  text-secondary md:text-lg lg:mx-10 lg:mt-10 lg:leading-7 ">
+              Given our experience in the trust industry, we see that people
+              consider trusts as: <br></br>
+              <br></br>- an estate planning tool for the wealthy individuals as
+              the starting price to establish a trust and the threshold in
+              maintaining a trust is generally high<br></br>
+              <br></br>- inconvenient and time consuming as there will usually
+              be multiple in-person/virtual meetings and calls with the service
+              providers to finalise trust documents<br></br>
+              <br></br>
+              The above factors have generally deterred many people from
+              approaching or considering establishing trusts. <br></br>
+              <br></br>
+              OneDay believes that trust is a very useful estate planning tool
+              and that it should be made accessible to as many people as
+              possible.
+            </p>
 
-                    - an estate planning tool for the wealthy individuals as the starting price to establish a trust and the threshold in maintaining a trust is generally high<br></br><br></br>
+            <h5 className="mx-5 mt-10 font-display text-3xl font-bold leading-8 text-purple md:text-5xl lg:text-4xl lg:leading-12">
+              What is OneDay planning to do?
+            </h5>
 
-                    - inconvenient and time consuming as there will usually be multiple in-person/virtual meetings and calls with the service providers to finalise trust documents<br></br><br></br>
+            <p className="text-md mx-5 mt-5 font-sans leading-5  text-secondary md:text-lg lg:mx-10 lg:mt-10 lg:leading-7 ">
+              OneDay will be launching an online platform providing everyone
+              accessibility to establish and execute trusts in an affordable,
+              quick and secured manner. <br></br>
+              <br></br>
+              Particularly, we understand that time is of the essence and
+              sometimes it is difficult to juggle around time. By signing up to
+              OneDay trust plans, you do not need to worry anymore about
+              spending time commuting or scheduling appointments as you can
+              conveniently establish your trusts from your home or anywhere else
+              you like and at any time.
+            </p>
 
-                    The above factors have generally deterred many people from approaching or considering establishing trusts. <br></br><br></br>
+            <h5 className="mx-5 mt-10 font-display text-3xl font-bold leading-8 text-purple md:text-5xl lg:text-4xl lg:leading-12">
+              After I sign up to a OneDay trust plan and sign my trust deed,
+              what do I do next? Do I need to look for a trustee?
+            </h5>
 
-                    OneDay believes that trust is a very useful estate planning tool and that it should be made accessible to as many people as possible. 
-
-
-                </p>
-                
-                <h5 className="font-display text-3xl font-bold leading-8 text-purple md:text-5xl lg:text-4xl lg:leading-12 mt-10 mx-5">
-                    What is OneDay planning to do?
-                </h5>
-
-                <p className="mx-5 mt-5 font-sans text-md leading-5  text-secondary md:text-lg lg:mx-10 lg:mt-10 lg:leading-7 ">
-                OneDay will be launching an online platform providing everyone accessibility to establish and execute trusts in an affordable, quick and secured manner. <br></br><br></br>
-
-                Particularly, we understand that time is of the essence and sometimes it is difficult to juggle around time.  By signing up to OneDay trust plans, you do not need to worry anymore about spending time commuting or scheduling appointments as you can conveniently establish your trusts from your home or anywhere else you like and at any time. 
-
-
-                </p>
-
-                <h5 className="font-display text-3xl font-bold leading-8 text-purple md:text-5xl lg:text-4xl lg:leading-12 mt-10 mx-5">
-                    After I sign up to a OneDay trust plan and sign my trust deed, what do I do next? Do I need to look for a trustee?  
-                </h5>
-
-                <p className="mx-5 mt-5 font-sans text-md leading-5  text-secondary md:text-lg lg:mx-10 lg:mt-10 lg:leading-7 mb-20">
-                    We think from your perspective and try to align everything for you, alleviating your stress and workload. OneDay not only provides you with trust documents, we also help you establish your trusts. <br></br><br></br>
-
-                    In order to effectively establish your trust, you will need to transfer initial funds to the trustee of your trust, otherwise, your trust is just a standby trust where no assets are being managed. By signing up to our trust plans, you do not need to look for a trustee because OneDay will act as the licensed and regulated trustee for your trust. <br></br><br></br>
-
-                    Hence, after you sign your trust deed generated through our OneDay platform, all you need to do is to transfer initial funds (e.g. currencies such as USD, CAD, HKD and SGD or stablecoins such as USDC and BUSD) to OneDay to effectively establish your trust. <br></br><br></br>
-
-                    OneDay will not be holding your funds. Instead, your currencies will be kept with world leading financial institutions and your stablecoins with our partner custodian that is licensed, regulated and has insurance coverage over US$500M. So you can rest assured that your funds are secured and protected at all times. <br></br><br></br>
-
-                    We adopt a multi-party signature (for accounts held with financial institutions) and multi-party computation (for accounts held with our partner custodian) processes so that no one individual could take control of your funds at all times and most importantly, there will always be someone to manage your trust funds. We will issue a further newsletter with more detailed information on how our trust accounts are managed. Look out for our newsletters!
-
-
-                </p>
-            </div>
+            <p className="text-md mx-5 mt-5 mb-20 font-sans  leading-5 text-secondary md:text-lg lg:mx-10 lg:mt-10 lg:leading-7">
+              We think from your perspective and try to align everything for
+              you, alleviating your stress and workload. OneDay not only
+              provides you with trust documents, we also help you establish your
+              trusts. <br></br>
+              <br></br>
+              In order to effectively establish your trust, you will need to
+              transfer initial funds to the trustee of your trust, otherwise,
+              your trust is just a standby trust where no assets are being
+              managed. By signing up to our trust plans, you do not need to look
+              for a trustee because OneDay will act as the licensed and
+              regulated trustee for your trust. <br></br>
+              <br></br>
+              Hence, after you sign your trust deed generated through our OneDay
+              platform, all you need to do is to transfer initial funds (e.g.
+              currencies such as USD, CAD, HKD and SGD or stablecoins such as
+              USDC and BUSD) to OneDay to effectively establish your trust.{" "}
+              <br></br>
+              <br></br>
+              OneDay will not be holding your funds. Instead, your currencies
+              will be kept with world leading financial institutions and your
+              stablecoins with our partner custodian that is licensed, regulated
+              and has insurance coverage over US$500M. So you can rest assured
+              that your funds are secured and protected at all times. <br></br>
+              <br></br>
+              We adopt a multi-party signature (for accounts held with financial
+              institutions) and multi-party computation (for accounts held with
+              our partner custodian) processes so that no one individual could
+              take control of your funds at all times and most importantly,
+              there will always be someone to manage your trust funds. We will
+              issue a further newsletter with more detailed information on how
+              our trust accounts are managed. Look out for our newsletters!
+            </p>
+          </div>
         </div>
       </main>
       <Footer />
